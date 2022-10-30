cb78b9f0a9140dcf768d24ca5ff16c8e3d7d4c9c
yogeshdhangar5213 <yogeshdhangar5213@gmail.com>
2022-10-29 23:58:53 -0700

    Added: Check Mobile Number

--- src/main/java/UserRegistration.java
+++ src/main/java/UserRegistration.java
@@ -1,16 +1,14 @@
 import java.util.Scanner;
 import java.util.regex.Pattern;
-
 public class UserRegistration {
     static void StringFormat(){
-        System.out.println("Enter The Email Id ");
+        System.out.println("Enter The Mobile Number");
         Scanner sc = new Scanner(System.in);
-        String Email = sc.next();
-        String CheckEmailId= ("^[a-z]{3}[.]?[@#$%]{1}[a-z]{2}[.]{1}[a-z]{2}$");
-        boolean valid = Pattern.compile(Email).matcher(CheckEmailId).matches();
+        String Number = sc.next();
+        String CheckEmailId= ("^[9]{1}[1]{1}[ ]{1}[0-9]{10}$");
+        boolean valid = Pattern.compile(Number).matcher(CheckEmailId).matches();
         System.out.println(valid);
     }
-
     public static void main(String[] args) {
         System.out.println("WelCome To Regex Problem");
         StringFormat();
